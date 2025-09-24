ef029e08382d59b451030f18376a5a244b4af745
Yusuf Mustahan <yumustyology@gmail.com>
2025-09-24 05:22:34 +0100

    Remove commented-out server initialization and error handling code

--- server/index.ts
+++ server/index.ts
@@ -36,32 +36,6 @@ app.use((req, res, next) => {
   next();
 });
 
-// (async () => {
-//   const server = await registerRoutes(app);
-
-//   app.use((err: any, _req: Request, res: Response, _next: NextFunction) => {
-//     const status = err.status || err.statusCode || 500;
-//     const message = err.message || "Internal Server Error";
-
-//     res.status(status).json({ message });
-//     throw err;
-//   });
-
-//   if (app.get("env") === "development") {
-//     await setupVite(app, server);
-//   } else {
-//     serveStatic(app);
-//   }
-
-// if (app.get("env") === "development") {
-//   const port = parseInt(process.env.PORT || '5000', 10);
-//   server.listen(port, "0.0.0.0", () => {
-//     log(`serving on port ${port}`);
-//   });
-// }
-
-// })();
-
 (async () => {
   const server = await registerRoutes(app);
 
